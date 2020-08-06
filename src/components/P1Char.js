800dadd0e4ccc1343f2aa49ae54b5bc47ce458d2
Erin Wrobel <ewrobel3@gatech.edu>
2020-08-05 20:52:44 -0400

    character creation, not stored atm

--- src/components/P1Char.js
+++ src/components/P1Char.js
@@ -6,14 +6,38 @@ const P1Char = () => {
    const [elemental, setElemental] = useState(localStorage.getItem("P1"));
 
    function construct() {
-      let el = {...elemental};
-      el.constructed = true;
-      setElemental(el);
+      let e1 = {...elemental};
+      e1.constructed = true;
+      e1.element = false;
+      e1.style = false;
+      e1.skill = false;
+      setElemental(e1);
+      localStorage.setItem("P1", {elemental});
+   }
+
+   function setElement(element) {
+      let e1 = {...elemental};
+      e1.element = element;
+      setElemental(e1);
+      localStorage.setItem("P1", {elemental});
+   }
+
+   function setStyle(style) {
+      let e1 = {...elemental};
+      e1.style = style;
+      setElemental(e1);
+      localStorage.setItem("P1", {elemental});
+   }
+
+   function setSkill(skill) {
+      let e1 = {...elemental};
+      e1.skill = skill;
+      setElemental(e1);
       localStorage.setItem("P1", {elemental});
    }
    
    return (
-      <div class="charcreation">
+      <div class="playerchars">
          <div id="blocks">
             {(!elemental || !elemental.constructed) && 
                <button id="constructors" onClick={() => construct()}>
@@ -25,43 +49,92 @@ const P1Char = () => {
                   </div>
                </button>
             }
+            {(elemental && elemental.constructed) && <div>
+               <div id="elements">
+                  {elemental.element !== "Water" && 
+                     <button className="Water" onClick={() => setElement("Water")}>Water</button>
+                  }
+                  {elemental.element !== "Earth" && 
+                     <button className="Earth" onClick={() => setElement("Earth")}>Earth</button>
+                  }
+                  {elemental.element !== "Fire" && 
+                     <button className="Fire" onClick={() => setElement("Fire")}>Fire</button>
+                  }
+                  {elemental.element !== "Air" && 
+                     <button className="Air" onClick={() => setElement("Air")}>Air</button>
+                  }
+               </div>
+               <div id="styles">
+                  {elemental.style !== "Aggressive" && 
+                     <button className="style" onClick={() => setStyle("Aggressive")}>Aggressive</button>
+                  }
+                  {elemental.style !== "Balanced" && 
+                     <button className="style" onClick={() => setStyle("Balanced")}>Balanced</button>
+                  }
+                  {elemental.style !== "Defensive" && 
+                     <button className="style" onClick={() => setStyle("Defensive")}>Defensive</button>
+                  }
+               </div>
+               <div id="skills">
+                  {elemental.skill !== "Power" && 
+                     <button className="skill" onClick={() => setSkill("Power")}>Power</button>
+                  }
+                  {elemental.skill !== "Accuracy" && 
+                     <button className="skill" onClick={() => setSkill("Accuracy")}>Accuracy</button>
+                  }
+                  {elemental.skill !== "Speed" && 
+                     <button className="skill" onClick={() => setSkill("Speed")}>Speed</button>
+                  }
+               </div>
+            </div>}
             
          </div>
          <div id="constructor">
+            <div className="label">Elemental Constructor</div>
             <div id="slot">
                {elemental && <div>
                   {elemental.constructed && 
                      <div id="constructorStart">
-                        {"elemental {"}
+                        {"elemental ("}
                      </div>
                   }
                   {elemental.element && 
-                     <div classname={elemental.element}>
+                     <div className={elemental.element}>
                         {elemental.element}
                      </div>
                   }
                   {elemental.style &&
-                     <div classname={elemental.style}>
+                     <div className="style">
                         {elemental.style}
                      </div>
                   }
                   {elemental.skill &&
-                     <div classname={elemental.skill}>
+                     <div className="skill">
                         {elemental.skill}
                      </div>
                   }
                   {elemental.constructed && 
                      <div id="constructorEnd">
-                        {"}"}
+                        {")"}
                      </div>
                   }
                </div>}
             </div>
          </div>
          <div id="elemental">
-
+            {elemental.constructed &&
+               <div id={elemental.element ? elemental.element : "blank"}>
+                  <div className="eyeball"></div>
+                  <div className="pupil"></div>
+                  <div id={elemental.style}></div>
+                  <div id={elemental.skill}></div>
+               </div>
+            }
+            <div id="podium"></div>
          </div>
-         <NavLink id="ccnext" class="button" to="/CharCreation">Done</NavLink>
+         {(elemental && elemental.constructed && elemental.element && elemental.style && elemental.skill) && 
+            <NavLink id="ccnext" class="button" to="/CharCreation">Done</NavLink>
+         }
       </div>
    );
 }
--- src/components/P2Char.js
+++ src/components/P2Char.js
@@ -1,13 +1,142 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { NavLink } from 'react-router-dom';
  
 const P2Char = () => {
-    return (
-        <div class="charcreation">
+   const [elemental, setElemental] = useState(localStorage.getItem("P1"));
+
+   function construct() {
+      let e1 = {...elemental};
+      e1.constructed = true;
+      e1.element = false;
+      e1.style = false;
+      e1.skill = false;
+      setElemental(e1);
+      localStorage.setItem("P1", {elemental});
+   }
+
+   function setElement(element) {
+      let e1 = {...elemental};
+      e1.element = element;
+      setElemental(e1);
+      localStorage.setItem("P1", {elemental});
+   }
+
+   function setStyle(style) {
+      let e1 = {...elemental};
+      e1.style = style;
+      setElemental(e1);
+      localStorage.setItem("P1", {elemental});
+   }
+
+   function setSkill(skill) {
+      let e1 = {...elemental};
+      e1.skill = skill;
+      setElemental(e1);
+      localStorage.setItem("P1", {elemental});
+   }
+   
+   return (
+      <div class="playerchars">
+         <div id="blocks">
+            {(!elemental || !elemental.constructed) && 
+               <button id="constructors" onClick={() => construct()}>
+                  <div id="constructorStart">
+                     {"elemental {"}
+                  </div>
+                  <div id="constructorEnd">
+                     {"}"}
+                  </div>
+               </button>
+            }
+            {(elemental && elemental.constructed) && <div>
+               <div id="elements">
+                  {elemental.element !== "Water" && 
+                     <button className="Water" onClick={() => setElement("Water")}>Water</button>
+                  }
+                  {elemental.element !== "Earth" && 
+                     <button className="Earth" onClick={() => setElement("Earth")}>Earth</button>
+                  }
+                  {elemental.element !== "Fire" && 
+                     <button className="Fire" onClick={() => setElement("Fire")}>Fire</button>
+                  }
+                  {elemental.element !== "Air" && 
+                     <button className="Air" onClick={() => setElement("Air")}>Air</button>
+                  }
+               </div>
+               <div id="styles">
+                  {elemental.style !== "Aggressive" && 
+                     <button className="style" onClick={() => setStyle("Aggressive")}>Aggressive</button>
+                  }
+                  {elemental.style !== "Balanced" && 
+                     <button className="style" onClick={() => setStyle("Balanced")}>Balanced</button>
+                  }
+                  {elemental.style !== "Defensive" && 
+                     <button className="style" onClick={() => setStyle("Defensive")}>Defensive</button>
+                  }
+               </div>
+               <div id="skills">
+                  {elemental.skill !== "Power" && 
+                     <button className="skill" onClick={() => setSkill("Power")}>Power</button>
+                  }
+                  {elemental.skill !== "Accuracy" && 
+                     <button className="skill" onClick={() => setSkill("Accuracy")}>Accuracy</button>
+                  }
+                  {elemental.skill !== "Speed" && 
+                     <button className="skill" onClick={() => setSkill("Speed")}>Speed</button>
+                  }
+               </div>
+            </div>}
+            
+         </div>
+         <div id="constructor">
+         <div className="label">Elemental Constructor</div>
+            <div id="slot">
+               {elemental && <div>
+                  {elemental.constructed && 
+                     <div id="constructorStart">
+                        {"elemental ("}
+                     </div>
+                  }
+                  {elemental.element && 
+                     <div className={elemental.element}>
+                        {elemental.element}
+                     </div>
+                  }
+                  {elemental.style &&
+                     <div className="style">
+                        {elemental.style}
+                     </div>
+                  }
+                  {elemental.skill &&
+                     <div className="skill">
+                        {elemental.skill}
+                     </div>
+                  }
+                  {elemental.constructed && 
+                     <div id="constructorEnd">
+                        {")"}
+                     </div>
+                  }
+               </div>}
+            </div>
+         </div>
+         <div id="elemental">
+            {elemental.constructed &&
+               <div id={elemental.element ? elemental.element : "blank"}>
+                  <div className="eyeball"></div>
+                  <div className="pupil"></div>
+                  <div id={elemental.style}></div>
+                  <div id={elemental.skill}></div>
+               </div>
+            }
+            <div id="podium"></div>
+         </div>
+         {(elemental && elemental.constructed && elemental.element && elemental.style && elemental.skill) && 
             <NavLink id="ccnext" class="button" to="/CharCreation">Done</NavLink>
-        </div>
-    );
+         }
+      </div>
+   );
 }
  
 export default P2Char;
